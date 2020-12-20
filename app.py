3d3de9bbe762e7566af2e1d3c8fc41d7387a4bfb
Sanjay S <scsewbh@gmail.com>
2020-12-20 14:39:52 -0500

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('static/index.html')
+    return render_template('.\static\index.html')
 
 
 if __name__ == '__main__':
