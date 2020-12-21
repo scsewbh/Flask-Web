c0783dd6dd39ad62eb85f4a8d704765c55a56096
Sanjay S <scsewbh@gmail.com>
2020-12-20 20:02:32 -0500

    fixed links on pages

--- app.py
+++ app.py
@@ -2,11 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('.\static\index.html')
+    return render_template('index.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/table')
+def table():
+    return render_template('table.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/forgot-password')
+def forgot_password():
+    return render_template('forgot-password.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
